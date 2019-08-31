e057ca4e615dde448563f6781da48494f93c85d4
dongfengjue <1009529808@qq.com>
2019-08-31 22:22:09 +0800

    通过adb 控制手机

--- os/open.py
+++ os/open.py
@@ -4,4 +4,8 @@ file_path = "/Users/chenbing/Documents/file/1.mp3"
 # subprocess.call(["open", file_path])
 
 cmd = "/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome /Users/chenbing/Documents/file/1.mp3"
-subprocess.call(cmd, shell=True) 
+# subprocess.call(cmd, shell=True) 
+
+# adb 连接手机 控制adb命令 控制手机
+adbShell = 'adb shell input keyevent 82'
+subprocess.call(adbShell, shell=True) 
