9c2afc79a06b5d3d0fb77c77399e06dcadd82cda
dongfengjue <1009529808@qq.com>
2019-09-01 08:19:19 +0800

    增加安装软件说明

--- image/read.py
+++ image/read.py
@@ -1,6 +1,10 @@
 from PIL import Image
 import pytesseract
 
+# brew install tesseract
+# 如中文需下载chi_sim.traineddata,将下载好的语言包放到/usr/local/Cellar/tesseract/3.05.01/share/tessdata/路径下。
+# https://pan.baidu.com/s/1J0HNoVhX8WexS_5r0k2jDw 密码: ywc3 语言包
+
 picPath = '/Users/chenbing/Documents/file/word.jpg'
 Image = Image.open(picPath)   # 打开图片
 text = pytesseract.image_to_string(Image,lang='chi_sim')  #使用简体中文解析图片
